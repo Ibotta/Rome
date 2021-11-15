87bc2db4ef03c48388ba9f57718804618619054b
Justin Vallely <justinvallely@gmail.com>
2021-11-15 12:57:55 -0700

    AT-2794 dSYMs not uploading to embrace (#1)
    * builds xcframeworks by default
    
    * linting
    
    * copies dSYMs to the correct folder inside each xcframework
    
    * more linting

--- lib/cocoapods-rome/post_install.rb
+++ lib/cocoapods-rome/post_install.rb
@@ -4,7 +4,7 @@ PLATFORMS = { 'iphonesimulator' => 'iOS',
               'appletvsimulator' => 'tvOS',
               'watchsimulator' => 'watchOS' }
 
-def build_for_iosish_platform(sandbox, build_dir, target, device, simulator, flags, configuration, build_xcframework=false)
+def build_for_iosish_platform(sandbox, build_dir, target, device, simulator, flags, configuration, build_xcframework = false)
   deployment_target = target.platform_deployment_target
   target_label = target.cocoapods_target_label
 
@@ -28,7 +28,7 @@ def build_for_iosish_platform(sandbox, build_dir, target, device, simulator, fla
   end
 end
 
-def build_for_macos_platform(sandbox, build_dir, target, flags, configuration, build_xcframework=false)
+def build_for_macos_platform(sandbox, build_dir, target, flags, configuration, build_xcframework = false)
   target_label = target.cocoapods_target_label
   xcodebuild(sandbox, target_label, flags, configuration)
 
@@ -41,8 +41,8 @@ def build_for_macos_platform(sandbox, build_dir, target, flags, configuration, b
   end
 end
 
-def xcodebuild(sandbox, target, sdk='macosx', deployment_target=nil, flags=nil, configuration)
-  args = %W(-project #{sandbox.project_path.realdirpath} -scheme #{target} -configuration #{configuration} -sdk #{sdk})
+def xcodebuild(sandbox, target, sdk = 'macosx', deployment_target = nil, flags = nil, configuration)
+  args = %W[-project #{sandbox.project_path.realdirpath} -scheme #{target} -configuration #{configuration} -sdk #{sdk}]
   args += flags unless flags.nil?
   platform = PLATFORMS[sdk]
   args += Fourflusher::SimControl.new.destination(:oldest, platform, deployment_target) unless platform.nil?
@@ -53,25 +53,29 @@ def build_universal_framework(device_lib, simulator_lib, build_dir, destination,
   device_executable = "#{device_lib}/#{module_name}"
   simulator_executable = "#{simulator_lib}/#{module_name}"
 
-  raise Pod::Informative, 'Framework executables were not found in the expected location.' unless File.file?(device_executable) && File.file?(simulator_executable)
+  unless File.file?(device_executable) && File.file?(simulator_executable)
+    raise Pod::Informative,
+          'Framework executables were not found in the expected location.'
+  end
 
   device_framework_lib = File.dirname(device_executable)
   lipo_log = `lipo -create -output #{destination} #{device_executable} #{simulator_executable}`
   puts lipo_log unless File.exist?(destination)
 
-  FileUtils.mv destination, device_executable, :force => true
-  FileUtils.mv device_framework_lib, build_dir, :force => true
+  FileUtils.mv destination, device_executable, force: true
+  FileUtils.mv device_framework_lib, build_dir, force: true
 end
 
 def build_xcframework(frameworks, build_dir, module_name)
   output = "#{build_dir}/#{module_name}.xcframework"
   return if File.exist?(output)
 
-  args = %W(-create-xcframework -output #{output})
+  args = %W[-create-xcframework -output #{output}]
 
   frameworks.each do |framework|
     return unless File.exist?(framework)
-    args += %W(-framework #{framework})
+
+    args += %W[-framework #{framework}]
   end
 
   puts "Building XCFramework for: #{module_name}"
@@ -89,15 +93,27 @@ def enable_debug_information(project_path, configuration)
   project.save
 end
 
-def copy_dsym_files(dsym_destination, configuration)
-  dsym_destination.rmtree if dsym_destination.directory?
-  platforms = ['iphoneos', 'iphonesimulator']
+def copy_dsym_files(destination, configuration)
+  platforms = ['iphoneos']
   platforms.each do |platform|
     dsym = Pathname.glob("build/#{configuration}-#{platform}/**/*.dSYM")
     dsym.each do |dsym|
-      destination = dsym_destination + platform
-      FileUtils.mkdir_p destination
-      FileUtils.cp_r dsym, destination, :remove_destination => true
+      dsym_basename = File.basename(dsym, '.framework.dSYM')
+      xcframework_path = File.join(destination, "#{dsym_basename}.xcframework")
+
+      next unless Dir.exist?(xcframework_path)
+
+      Dir.children(xcframework_path).each do |child|
+        next unless File.directory? File.join(xcframework_path, child)
+        next unless child.include? 'ios-arm64'
+        next if child.include? '-simulator'
+        next if child.include? '-maccatalyst'
+
+        dsym_destination = File.join(xcframework_path, child, 'dSYMs')
+        puts "Copying dsym to #{dsym_destination}"
+
+        FileUtils.cp_r dsym, dsym_destination, remove_destination: true
+      end
     end
   end
 end
@@ -105,19 +121,19 @@ end
 Pod::HooksManager.register('cocoapods-rome', :post_install) do |installer_context, user_options|
   enable_dsym = user_options.fetch('dsym', true)
   configuration = user_options.fetch('configuration', 'Debug')
-  build_xcframework = user_options.fetch('xcframework', false)
+  build_xcframework = user_options.fetch('xcframework', true)
 
   flags = []
 
   # Setting SKIP_INSTALL=NO to access the built frameworks inside the archive created
   # instead of searching in Xcode’s default derived data folder
-  flags << "SKIP_INSTALL=NO" if build_xcframework
+  flags << 'SKIP_INSTALL=NO' if build_xcframework
 
   # Use custom flags passed via user options, if any
-  flags += user_options["flags"] if user_options["flags"]
+  flags += user_options['flags'] if user_options['flags']
 
-  if user_options["pre_compile"]
-    user_options["pre_compile"].call(installer_context)
+  if user_options['pre_compile']
+    user_options['pre_compile'].call(installer_context)
   end
 
   sandbox_root = Pathname(installer_context.sandbox_root)
@@ -146,7 +162,7 @@ Pod::HooksManager.register('cocoapods-rome', :post_install) do |installer_contex
 
   # Make sure the device target overwrites anything in the simulator build, otherwise iTunesConnect
   # can get upset about Info.plist containing references to the simulator SDK
-  build_type = build_xcframework ? "xcframework" : "framework"
+  build_type = build_xcframework ? 'xcframework' : 'framework'
   frameworks = Pathname.glob("build/*/*/*.#{build_type}").reject { |f| f.to_s =~ /Pods[^.]+\.#{build_type}/ }
   frameworks += Pathname.glob("build/*.#{build_type}").reject { |f| f.to_s =~ /Pods[^.]+\.#{build_type}/ }
 
@@ -168,19 +184,18 @@ Pod::HooksManager.register('cocoapods-rome', :post_install) do |installer_contex
   frameworks.uniq!
   resources.uniq!
 
-  Pod::UI.puts "Copying #{frameworks.count} #{'frameworks'.pluralize(frameworks.count)} " \
-    "to `#{destination.relative_path_from Pathname.pwd}`"
+  Pod::UI.puts "Copying #{frameworks.count} #{'frameworks'.pluralize(frameworks.count)} to `#{destination.relative_path_from Pathname.pwd}`"
 
   FileUtils.mkdir_p destination
   (frameworks + resources).each do |file|
-    FileUtils.cp_r file, destination, :remove_destination => true
+    FileUtils.cp_r file, destination, remove_destination: true
   end
 
-  copy_dsym_files(sandbox_root.parent + 'dSYM', configuration) if enable_dsym
+  copy_dsym_files(destination, configuration) if enable_dsym
 
   build_dir.rmtree if build_dir.directory?
 
-  if user_options["post_compile"]
-    user_options["post_compile"].call(installer_context)
+  if user_options['post_compile']
+    user_options['post_compile'].call(installer_context)
   end
 end
